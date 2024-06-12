2324cd9f03ff53c9968bc767552dad94c708e888
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-06-12 19:24:07 +0200

    feat: fizzbuzz view

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,15 @@
 
 public abstract class Program {
 	private static void Main() {
+		string? line = Console.ReadLine();
+		FizzBuzz fizzBuzz = new();
 
+		while (line != null) {
+			if (!int.TryParse(line, out int number))
+				break;
+			Console.WriteLine(fizzBuzz.Convert(number));
+
+			line = Console.ReadLine();
+		}
 	}
 }
